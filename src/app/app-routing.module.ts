3a1a7f2e3e931beac835ad955083ff32e0d22e22
SimpleMax05 <onpustmacs@yandex.by>
2023-02-16 22:16:33 +0300

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Test1Component } from './test1/test1.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: 'first-component', component: Test1Component },
+    { path: 'second-component', component: Test1Component },
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
